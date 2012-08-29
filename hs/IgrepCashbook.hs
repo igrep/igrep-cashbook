ee6ff7a2c960aa6357a81e06ffdaefa4f7b6c0c8
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-08-30 06:17:11 +0900

    add IgrepCashbook.hs but doesn't export anything yet

---
+++ hs/IgrepCashbook.hs
@@ -0,0 +1,23 @@
+module IgrepCashbook where
+
+import Text.Regex
+
+-- General function
+equalBy :: (Eq b) => ( a -> b ) -> a -> a -> Bool
+equalBy f a1 a2 = f a1 == f a2
+
+type Item = [String]
+
+parseLine :: String -> Item
+parseLine "" = [""]
+parseLine s@('#':_) = [s]
+parseLine s = splitRegex r s
+  where
+    r = mkRegex "  +|\t"
+
+getDate :: Item -> String
+getDate = (!!0)
+
+isComment :: Item -> Bool
+isComment [('#':_)] = True
+isComment _ = False
