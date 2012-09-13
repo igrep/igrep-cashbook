6720a3106c1b85b4ed63e0a488459f4d96220b1d
Yamamoto Yuji <whosekiteneverfly _at_ gmail.com>
2012-09-13 22:50:26 +0900

    add bad string literal of IgrepCashbook2.hs
    fix Rakefile not to override sum(rb) in my/money

--- hs/IgrepCashbook2.hs
+++ hs/IgrepCashbook2.hs
@@ -20,6 +20,7 @@ import Text.Regex.Posix
 
 type Item = Old.Item
 
+isComment :: Item -> Bool
 isComment = Old.isComment
 
 isItemLine :: String -> Bool
@@ -30,12 +31,17 @@ parseItemLine :: String -> Item
 parseItemLine (' ':s) = Old.parseLine s
 parseItemLine s = [s]
 
+emptyItem :: String
 emptyItem = "invalid item: empty item. no data given"
+noPriceAndGroup :: String
 noPriceAndGroup = "invalid item: neither price nor group given"
+noGroup :: String
 noGroup = "invalid item: no group given"
+invalidPrice :: String
 invalidPrice = "invalid item: malformed price"
 
-priceRegex = "^\+?[1-9][_,0-9]*$"
+priceRegex :: String
+priceRegex = "^\\+?[1-9][_,0-9]*$"
 
 validateItem :: Item -> Either String Item
 validateItem [] = Left emptyItem
