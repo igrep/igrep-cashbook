a00475ae3e00160ec5dfce73128d7577653896a5
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2017-01-14 13:09:13 +0900

    (WIP) Add C# version

---
+++ cs/SumIgrepCashbook/SumIgrepCashbook/CashBookItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SumIgrepCashbook
+{
+    class CashBookItem
+    {
+        private static readonly Regex itemLineRegex = new Regex(@"^ [\S ]+  (\+?)(\d[\d,_]*)  (\S+)");
+        private static readonly Regex priceDelimiterRegex = new Regex(@"[,_]");
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public bool IsIncome { get; set; }
+
+        public static bool IsItemLine( string line )
+        {
+            return itemLineRegex.IsMatch(line);
+        }
+
+        public static CashBookItem ParseLine(string line)
+        {
+            var match = itemLineRegex.Match(line);
+            if (match.Success)
+            {
+                var groups = match.Groups;
+                return new CashBookItem
+                {
+                    IsIncome = !( groups[1].Value.Equals(String.Empty) ),
+                    Price = ParsePrice( groups[2].Value ),
+                    Category = groups[3].Value
+                };
+            }
+            else
+            {
+                throw new InvalidItemException(String.Format("Invalid line: {0}", line));
+            }
+        }
+
+        private static int ParsePrice(string priceField)
+        {
+            return int.Parse(priceDelimiterRegex.Replace(priceField, ""));
+        }
+    }
+
+    class InvalidItemException : Exception
+    {
+        public InvalidItemException( string message ): base( message )
+        {
+        }
+    }
+}
---
+++ cs/SumIgrepCashbook/SumIgrepCashbook/Sum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SumIgrepCashbook
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            foreach ( var fileName in args )
+            {
+                var itemsForSum = from line in File.ReadLines(fileName)
+                                  where CashBookItem.IsItemLine(line)
+                                  select CashBookItem.ParseLine(line);
+                var categorySums = from item in itemsForSum
+                                   group item by item.Category into byCategory
+                                   select new
+                                   {
+                                       Category = byCategory.Key,
+                                       Sum = byCategory.Sum( g => g.Price )
+                                   };
+
+                var wholeSum = (from c in categorySums select c.Sum).Sum();
+                foreach (var c in categorySums.OrderBy( c => c.Sum ))
+                {
+                    Console.WriteLine("{0}\t{1,8:D}", c.Category, c.Sum);
+                }
+                Console.WriteLine("合計\t{0,8:D}", wholeSum);
+            }
+        }
+    }
+}
