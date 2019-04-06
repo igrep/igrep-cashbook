f40891927763685262feb5b3a6f077a02df7475c
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-04-06 19:45:43 +0900

    Build haskell library with ahc-cabal

---
+++ hs2/build-asterius.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+mkdir -p build/
+
+sudo docker run -it \
+  -v `pwd`:/mirror \
+  -v `pwd`/build:/root/.cabal \
+  terrorjack/asterius:wip-update-ghc \
+  bash -c 'cd /mirror/ && ahc-cabal new-build --project-file=ahc-cabal.project'
+  #bash -c 'ahc-cabal new-update && cd /mirror/ && ahc-cabal new-build --project-file=ahc-cabal.project'
+  #> ahc-cabal-new-build.log 2>&1
