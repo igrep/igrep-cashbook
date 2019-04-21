cbc60eb9643a64cf6b00ac0e8fa3d270d9fdb508
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-04-21 21:34:14 +0900

    Fix encoding issue in Windows

--- hs2/app/Main.hs
+++ hs2/app/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
 import qualified Control.Foldl      as Foldl
+import qualified Data.Text.Lazy     as Lazy
 import qualified Data.Text.Lazy.IO  as Lazy
 import           System.Environment (getArgs)
-import           System.IO          (stderr)
+import           System.IO          (IOMode (ReadMode), hSetEncoding, openFile,
+                                     stderr, utf8)
 
 import qualified IgrepCashbook
 
@@ -12,6 +14,13 @@ main :: IO ()
 main = do
   (err, out) <-
     IgrepCashbook.summaryToConsoleOutput
-      <$> (Foldl.foldM (IgrepCashbook.buildSummary Lazy.readFile) =<< getArgs)
+      <$> (Foldl.foldM (IgrepCashbook.buildSummary readUtf8File) =<< getArgs)
   Lazy.putStr out
   Lazy.hPutStr stderr err
+
+
+readUtf8File :: FilePath -> IO Lazy.Text
+readUtf8File path = do
+  hd <- openFile path ReadMode
+  hSetEncoding hd utf8
+  Lazy.hGetContents hd
